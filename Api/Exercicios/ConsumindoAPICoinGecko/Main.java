e50c118eb5259ccecbadc7ba7501aa9975122c2c
Matheus Antero <101403401+Anterosuehtam@users.noreply.github.com>
2026-03-12 22:50:06 -0300

    adiciona exercicio de API

--- Api/Exercicios/ConsumindoAPICoinGecko/Main.java
+++ Api/Exercicios/ConsumindoAPICoinGecko/Main.java
@@ -1,5 +1,4 @@
-package Api.Exercicios.Exercicio01;
-
+package Api.Exercicios.ConsumindoAPICoinGecko;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -7,22 +6,22 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-/*Crie um programa em Java que utilize as classes HttpClient, HttpRequest e HttpResponse
-para fazer uma consulta à API do Google Books. Solicite ao usuário que insira o título
-de um livro, e exiba as informações disponíveis sobre o livro retornado pela API.*/
+/*Crie um programa Java que utiliza as classes HttpClient, HttpRequest e HttpResponse para
+fazer uma consulta à API CoinGecko e exiba a cotação atual de uma criptomoeda escolhida pelo usuário.*/
 public class Main {
     static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Insira o nome do livro que deseja obter informações: ");
-        var busca = scanner.nextLine();
+        System.out.println("Insira uma criptomoeda para exibir a cotação atual: ");
+        String criptoNome = scanner.nextLine();
 
-        var endereco = "https://www.googleapis.com/books/v1/volumes?q=" + busca + "&key=AIzaSyCCBwAjCkUUG5w1z2MFtMC2dPY-2dQb8Ts";
+        String endereco = "https://api.coingecko.com/api/v3/simple/price?ids=" + criptoNome + "&vs_currencies=usd";
 
         // Criando um objeto da classe HttpClient
         HttpClient client = HttpClient.newHttpClient();
         // Criando um objeto da classe Request
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+        // Criando um objeto da classe Response que utiliza o metodo send da classe Client
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
