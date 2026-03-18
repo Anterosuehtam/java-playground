c9f385532a121f20f8dfb7b8618d6832d5b5e764
Matheus Antero <101403401+Anterosuehtam@users.noreply.github.com>
2026-03-17 23:08:44 -0300

    adicionando exercicios de trycatch e de serialização para json

---
+++ trycatch/Exercicio04/ErroDeConversaoDeDadosException.java
@@ -0,0 +1,9 @@
+package trycatch.Exercicio04;
+
+public class ErroDeConversaoDeDadosException extends RuntimeException{
+    private String mensagem;
+
+    public ErroDeConversaoDeDadosException(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
---
+++ trycatch/Exercicio05/Main.java
@@ -0,0 +1,24 @@
+package trycatch.Exercicio05;
+
+import com.google.gson.Gson;
+import trycatch.Exercicio04.ErroDeConversaoDeDadosException;
+
+/*Defina uma classe chamada Veiculo com os atributos necessários.
+Em seguida, crie um programa que instancia um objeto Veiculo, serializa
+esse objeto para JSON usando a biblioteca Gson e imprime o resultado.*/
+public class Main {
+    static void main(String[] args) {
+    Veiculo veiculo1 = new Veiculo("Prata", 2026, "Sedan");
+    try {
+        Gson gson = new Gson().newBuilder()
+                .setPrettyPrinting()
+                .create();
+
+        String json = gson.toJson(veiculo1);
+        System.out.println(json);
+    }catch (ErroDeConversaoDeDadosException e){
+        throw new ErroDeConversaoDeDadosException("Não foi possível converter os dados dos atributos para JSON");
+    }
+
+    }
+}
